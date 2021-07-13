74134fe32c7b3b257ba8c36de1019bac4b42b360
PalwashaAwan23 <82227478+PalwashaAwan23@users.noreply.github.com>
2021-07-13 18:23:23 +0100

    Add files via upload

---
+++ gan_training.py
@@ -0,0 +1,20 @@
+import gan_images
+import gan_model
+
+class Training:
+    def train_generator(self):
+        super(gan_model.gen())
+        return True
+
+    def train_dsicriminator(self):
+        super(gan_model.dis())
+        return True
+
+
+
+
+
+
+
+gen = train_generator()
+dis = train_discriminator()
